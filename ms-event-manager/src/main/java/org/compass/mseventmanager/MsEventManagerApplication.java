1372fad36ecdd2a6c05513493e937192086bd7a5
Gabriel do nascimento carvalho <nascimentocarvalho894@gmail.com>
2025-01-24 21:04:37 -0300

    Projeto criado

---
+++ ms-event-manager/src/main/java/org/compass/mseventmanager/MsEventManagerApplication.java
@@ -0,0 +1,13 @@
+package org.compass.mseventmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEventManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsEventManagerApplication.class, args);
+    }
+
+}
---
+++ ms-event-manager/src/test/java/org/compass/mseventmanager/MsEventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.compass.mseventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
